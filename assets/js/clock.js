471a2a428f1fbb742f76567c5c8fcbda48dbc1f9
Andy Lech <github@andylech.com>
2024-02-12 19:42:32 -0500

    Overhaul countdown timer to fix iOS NaNs showing up in countdown clock

--- assets/js/clock.js
+++ assets/js/clock.js
@@ -4,28 +4,31 @@
 // 1. Fixed typo ("Search") in comment
 // 2. Changed getTimeRemaining to return all zeroes for past deadlines
 // 3. Set UTC date/time for Orlando Code Camp 2024
+// 4. Pass start and end ticks into getTimeRemaining
+// 5. Better variable names (t => time, total => ticks, etc.)
+// 6. Add integer checks to initializeClock and getTimeRemaining
+// 7. Added temporary interim fields for debugging
 
-function getTimeRemaining(endtime) {
-	var endDate = Date.parse(endtime);
-  var now = Date.parse(new Date());
-
-  if (now >= endDate)
+function getTimeRemaining(startTick, endTick) {
+  if (!Number.isInteger(startTick)
+      || !Number.isInteger(endTick)
+      || startTick >= endTick)
   	return {
-    	total: 0,
+    	ticks: 0,
       days: 0,
       hours: 0,
       minutes: 0,
       seconds: 0
     };
 
-  var t = endDate - now;
-  var seconds = Math.floor(t / 1000 % 60);
-  var minutes = Math.floor(t / 1000 / 60 % 60);
-  var hours = Math.floor(t / (1000 * 60 * 60) % 24);
-  var days = Math.floor(t / (1000 * 60 * 60 * 24));
+  var ticks = endTick - startTick;
+  var days = Math.floor(ticks / (1000 * 60 * 60 * 24));
+  var hours = Math.floor(ticks / (1000 * 60 * 60) % 24);
+  var minutes = Math.floor(ticks / 1000 / 60 % 60);
+  var seconds = Math.floor(ticks / 1000 % 60);
 
   return {
-    total: t,
+    ticks: ticks,
     days: days,
     hours: hours,
     minutes: minutes,
@@ -33,42 +36,43 @@ function getTimeRemaining(endtime) {
   };
 }
 
-function initializeClock(id, endtime) {
+function initializeClock(id, endTick) {
+  if (!Number.isInteger(endTick))
+    return;
+
   var clock = document.getElementById(id);
-  var daysSpan = clock.querySelector('.days');
-  var hoursSpan = clock.querySelector('.hours');
-  var minutesSpan = clock.querySelector('.minutes');
-  var secondsSpan = clock.querySelector('.seconds');
+  var daysSpan = clock.querySelector('#days');
+  var hoursSpan = clock.querySelector('#hours');
+  var minutesSpan = clock.querySelector('#minutes');
+  var secondsSpan = clock.querySelector('#seconds');
+  var startTimeSpan = clock.querySelector('#start');
+  var endTimeSpan = clock.querySelector('#end');
+  var ticksSpan = clock.querySelector('#ticks');
+
+  endTimeSpan.innerHTML = new Date(endTick).toLocaleString('en-US');
 
   function updateClock() {
-    var t = getTimeRemaining(endtime);
+    var startTick = new Date().getTime();
+    var time = getTimeRemaining(startTick,endTick);
 
-    daysSpan.innerHTML = t.days;
-    hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
-    minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
-    secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
+    daysSpan.innerHTML = time.days;
+    hoursSpan.innerHTML = ('0' + time.hours).slice(-2);
+    minutesSpan.innerHTML = ('0' + time.minutes).slice(-2);
+    secondsSpan.innerHTML = ('0' + time.seconds).slice(-2);
+    startTimeSpan.innerHTML = new Date(startTick).toLocaleString('en-US');
+    ticksSpan.innerHTML = time.ticks;
 
-    if (t.total <= 0) {
-      clearInterval(timeinterval);
+    if (time.ticks <= 0) {
+      clearInterval(timeInterval);
     }
   }
 
   updateClock();
-  var timeinterval = setInterval(updateClock, 1000);
+  var timeInterval = setInterval(updateClock, 1000);
 }
 
-// example 15 days out
-// var deadline = new Date(Date.parse(new Date()) + 15 * 24 * 60 * 60 * 1000);
-// people in different timezones will see different dates unless you use UTC
-// Search your timezone in UTC
-// then use this format:
-// var deadline = new Date(Date.parse('19 August 2017 19:15:00Z'));
-
-// if (new Date() > deadline) {
-//   alert('COUNTDOWN COMPLETE! \n Some Call to Action!!!');
-// }
-
 // UTC equivalent of "Sat Feb 24 2024 07:30:00 GMT-0500 (Eastern Standard Time)"
-var deadline = new Date(Date.parse('24 February 2024 12:30:00Z'));
+// People in different timezones will see different dates unless you use UTC
+var endTick = new Date(Date.UTC(2024, 1, 24, 12, 30, 0, 0)).getTime();
 
-initializeClock('clockdiv', deadline);
+initializeClock('clock-div', endTick);
